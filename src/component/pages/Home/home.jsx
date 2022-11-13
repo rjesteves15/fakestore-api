6dc8541a71540fc399c3733213041694c4ec02c0
rchllstsvs <rochellejoan.p.esteves@gmail.com>
2022-11-13 23:25:57 +0800

    display product in home component

--- src/component/pages/Home/home.jsx
+++ src/component/pages/Home/home.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect}from 'react';
+import './home.scss'
 
 function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+    .then(data => setData(data))
+  })
+
+
   return (
-    <div>Home</div>
+    <div className='home__container'>
+
+
+        <div className='data__container'>
+            {
+              data.map((item, index) => (
+                  <img key={index} src={item.image}/>   
+              ))
+            }
+        </div>
+    </div>
   )
 }
 
