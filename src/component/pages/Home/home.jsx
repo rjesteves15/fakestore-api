7c8211acb1a79a116f2a38b125939043ed4512d6
rchllstsvs <rochellejoan.p.esteves@gmail.com>
2022-11-17 22:42:27 +0800

    added filter category functionality

--- src/component/pages/Home/home.jsx
+++ src/component/pages/Home/home.jsx
@@ -4,32 +4,65 @@ import useFetch from '../../useFetch';
 import './home.scss'
 
 function Home() {
-  const [data] = useFetch("https://fakestoreapi.com/products")
+  const [newData, setNewData] = useState([]);
+  const [viewItem, setViewItem] = useState("")
+  const [data] = useFetch("https://fakestoreapi.com/products");
 
+  const [category] = useFetch("https://fakestoreapi.com/products/categories")
+
+  const handleCategory = () => {
+      fetch(`https://fakestoreapi.com/products/category/${viewItem}`)
+      .then(res => res.json())
+      .then(data => setNewData(data));
+  }
 
   return (
     <div className='home__container'>
 
+    <div className="category__container">
+            {category.map((i) => (
+              <button onClick={() => handleCategory(setViewItem(i))}>{i}</button>
+            ))}
+          </div>
 
-<div className="data__container">
-        {data.map((item, index) => {
-          const { id, price, image, title, rating } = item;
+        <div className="data__container">
+                { newData.length === 0 ?
+                data.map((item, index) => {
+                  const { id, price, image, title, rating } = item;
 
-          return (
-            <div className="card">
-              <img key={index} src={item.image} />
+                  return (
+                    <div className="card">
+                      <img key={index} src={item.image} />
 
-              <div className="item__information">
-                {/* <p>{title}</p> */}
-                <h3>${price}</h3>
-                <Link to={`/product/${item.id}`}>
-                    View 
-                </Link>
+                      <div className="item__information">
+                        {/* <p>{title}</p> */}
+                        <h3>${price}</h3>
+                        <Link to={`/product/${item.id}`}>
+                            View 
+                        </Link>
+                      </div>
+                    </div>
+                  );
+                }) :
+                newData.map((item, index) => {
+                  const { id, price, image, title, rating } = item;
+
+                  return (
+                    <div className="card">
+                      <img key={index} src={item.image} />
+
+                      <div className="item__information">
+                        {/* <p>{title}</p> */}
+                        <h3>${price}</h3>
+                        <Link to={`/product/${item.id}`}>
+                            View 
+                        </Link>
+                      </div>
+                    </div>
+                  );
+                })
+                }
               </div>
-            </div>
-          );
-        })}
-      </div>
     </div>
   )
 }
